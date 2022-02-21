382ca8f79cf0dc1a13ccf1dc6210814e4cf91cf9
Latrylizer <latryll.alimes@coreassist.com>
2022-02-22 00:01:35 +0800

    fix onplay event in youtube player component

--- src/feature/YoutubePlayer.tsx
+++ src/feature/YoutubePlayer.tsx
@@ -2,32 +2,30 @@ import React, { Component } from 'react'
 import YouTube from 'react-youtube'
 
 class YoutubePlayer extends Component {
-    render() {
-        interface ScreenSize {
-            width: string
-            height: string
-            playerVars: object
-        }
-
-        const opts: ScreenSize = {
-            height: '720',
-            width: '100%',
-            playerVars: {
-                // https://developers.google.com/youtube/player_parameters
-                autoplay: 1,
-            },
-        }
-
-        return (
-            <YouTube videoId="GUE7vIGmugk" opts={opts} onReady={this.onPlay} />
-        )
+  render() {
+    interface ScreenSize {
+      width: string
+      height: string
+      playerVars: object
     }
 
-    onPlay(event) {
-        // access to player in all event handlers via event.target
-        event.target.mute()
-        event.target.playVideo()
+    const opts: ScreenSize = {
+      height: '720',
+      width: '100%',
+      playerVars: {
+        // https://developers.google.com/youtube/player_parameters
+        autoplay: 1,
+      },
     }
+
+    return <YouTube videoId="GUE7vIGmugk" opts={opts} onReady={this.onPlay} />
+  }
+
+  onPlay(event: { target: { mute: () => void; playVideo: () => void } }) {
+    // access to player in all event handlers via event.target
+    event.target.mute()
+    event.target.playVideo()
+  }
 }
 
 export { YoutubePlayer }
